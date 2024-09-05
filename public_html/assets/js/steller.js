d71633d564e6a70ac89a08e05e9c5e9278e346a0
Rayan436 <emailtoshirinakther@gmail.com>
2024-09-05 07:33:24 +0600

    fixed

--- public_html/assets/js/steller.js
+++ public_html/assets/js/steller.js
@@ -1,16 +1,4 @@
-/*!
-=========================================================
-* Steller Landing page
-=========================================================
 
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 
 // smooth scroll
 $(document).ready(function(){
@@ -27,6 +15,6 @@ $(document).ready(function(){
 			}, 700, function(){
 				window.location.hash = hash;
 			});
-      	} 
+      	}
     });
 });
